e5b6f3ce171f973c88ad1cb2b2d785f850926d51
jim-ww <jim.w2610@proton.me>
2026-08-15 13:08:18 +0200

    add podman

--- flake.nix
+++ flake.nix
@@ -69,131 +69,134 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = {
-    self,
-    nixpkgs,
-    nur,
-    #nixpkgs-stable,
-    home-manager,
-    stylix,
-    sops-nix,
-    ...
-  } @ inputs: let
-    user = "jim";
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    #stablePkgs = nixpkgs-stable.legacyPackages.${system};
-    commonModules = [
-      ./prefs.nix
-      ./modules/bluetooth.nix
-      ./modules/docker.nix
-      ./modules/earlyoom.nix
-      ./modules/firewall.nix
-      ./modules/graphics.nix
-      ./modules/pipewire.nix
-      ./modules/autologin.nix
-      ./modules/stylix.nix
-      ./modules/japanese.nix
-      ./modules/user.nix
-      ./modules/sops.nix
-      ./modules/tlp.nix
-      ./modules/nix-ld.nix
-      ./modules/games.nix
-      ./modules/wireguard.nix
-      ./modules/keyring.nix
-      ./modules/xdg.nix
-      ./modules/ollama.nix
-      nur.modules.nixos.default
-      stylix.nixosModules.stylix
-      sops-nix.nixosModules.sops
-      inputs.kickstart-nixvim.nixosModules.default
-      {
-        programs.nixvim.enable = true;
-        # with stablePkgs;
-        packages = [
-          inputs.nihongo.packages.${system}.default
-          inputs.pomodoro-go.packages.${system}.default
-          inputs.shiraberu.packages.${system}.default
-          inputs.charshare.packages.${system}.default
-          inputs.itpec-sensei.packages.${system}.default
-          inputs.gowebwrap.packages.${system}.default
-          inputs.gtr.packages.${system}.default
-          inputs.anitui.packages.${system}.default
-          inputs.kage.packages.${system}.default
-        ];
-      }
-      home-manager.nixosModules.home-manager
-      {
-        home-manager = {
-          useGlobalPkgs = true;
-          useUserPackages = true;
-          backupFileExtension = "backup";
-          users.${user} = {config, ...}: {
-            imports = [
-              ./prefs.nix
-              ./modules/home/git.nix
-              ./modules/home/sway.nix
-              ./modules/home/xdg.nix
-              ./modules/home/gpg.nix
-              ./modules/home/mpd.nix
-              ./modules/home/lf.nix
-              ./modules/home/battery-low.nix
-              ./modules/home/librewolf
-              ./modules/home/distrobox.nix
-              ./modules/home/jujutsu.nix
-              ./modules/home/zathura.nix
-              ./modules/home/direnv.nix
-              ./modules/home/tmux.nix
-              ./modules/home/fzf.nix
-              ./modules/home/tealdeer.nix
-              ./modules/home/protonvpn.nix
-              ./modules/home/go.nix
-              ./modules/home/servers-healthcheck.nix
-              ./modules/home/bash.nix
-              ./modules/home/bwrap.nix
-            ];
-            stylix.targets.fzf.enable = false;
-            dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
-            programs.btop.enable = true;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nur,
+      #nixpkgs-stable,
+      home-manager,
+      stylix,
+      sops-nix,
+      ...
+    }@inputs:
+    let
+      user = "jim";
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      #stablePkgs = nixpkgs-stable.legacyPackages.${system};
+      commonModules = [
+        ./prefs.nix
+        ./modules/bluetooth.nix
+        ./modules/docker.nix
+        ./modules/earlyoom.nix
+        ./modules/firewall.nix
+        ./modules/graphics.nix
+        ./modules/pipewire.nix
+        ./modules/autologin.nix
+        ./modules/stylix.nix
+        ./modules/japanese.nix
+        ./modules/user.nix
+        ./modules/sops.nix
+        ./modules/tlp.nix
+        ./modules/nix-ld.nix
+        ./modules/games.nix
+        ./modules/wireguard.nix
+        ./modules/keyring.nix
+        ./modules/xdg.nix
+        ./modules/ollama.nix
+        ./modules/podman.nix
+        nur.modules.nixos.default
+        stylix.nixosModules.stylix
+        sops-nix.nixosModules.sops
+        inputs.kickstart-nixvim.nixosModules.default
+        {
+          programs.nixvim.enable = true;
+          # with stablePkgs;
+          packages = [
+            inputs.nihongo.packages.${system}.default
+            inputs.pomodoro-go.packages.${system}.default
+            inputs.shiraberu.packages.${system}.default
+            inputs.charshare.packages.${system}.default
+            inputs.itpec-sensei.packages.${system}.default
+            inputs.gowebwrap.packages.${system}.default
+            inputs.gtr.packages.${system}.default
+            inputs.anitui.packages.${system}.default
+            inputs.kage.packages.${system}.default
+          ];
+        }
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            backupFileExtension = "backup";
+            users.${user} = { config, ... }: {
+              imports = [
+                ./prefs.nix
+                ./modules/home/git.nix
+                ./modules/home/sway.nix
+                ./modules/home/xdg.nix
+                ./modules/home/gpg.nix
+                ./modules/home/mpd.nix
+                ./modules/home/lf.nix
+                ./modules/home/battery-low.nix
+                ./modules/home/librewolf
+                ./modules/home/distrobox.nix
+                ./modules/home/jujutsu.nix
+                ./modules/home/zathura.nix
+                ./modules/home/direnv.nix
+                ./modules/home/tmux.nix
+                ./modules/home/fzf.nix
+                ./modules/home/tealdeer.nix
+                ./modules/home/protonvpn.nix
+                ./modules/home/go.nix
+                ./modules/home/servers-healthcheck.nix
+                ./modules/home/bash.nix
+                ./modules/home/bwrap.nix
+              ];
+              stylix.targets.fzf.enable = false;
+              dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+              programs.btop.enable = true;
 
-            # TODO why stylix doesnt set that?
-            gtk.gtk3.extraConfig = {
-              gtk-application-prefer-dark-theme = 1;
+              # TODO why stylix doesnt set that?
+              gtk.gtk3.extraConfig = {
+                gtk-application-prefer-dark-theme = 1;
+              };
+
+              home.packages = config.packages;
+              home.sessionVariables = config.env;
+              home.shellAliases = config.shellAliases;
+
+              home.username = config.user;
+              home.homeDirectory = "/home/${config.user}";
+              home.stateVersion = "24.05";
+              #_module.args.stablePkgs = stablePkgs;
             };
-
-            home.packages = config.packages;
-            home.sessionVariables = config.env;
-            home.shellAliases = config.shellAliases;
-
-            home.username = config.user;
-            home.homeDirectory = "/home/${config.user}";
-            home.stateVersion = "24.05";
-            #_module.args.stablePkgs = stablePkgs;
           };
+          nixpkgs.overlays = [
+            nur.overlays.default
+            inputs.ani-cli.overlays.default
+            inputs.ollama-local-websearch.overlays.default
+          ];
+        }
+      ];
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit
+            inputs
+            system
+            self
+            ;
         };
-        nixpkgs.overlays = [
-          nur.overlays.default
-          inputs.ani-cli.overlays.default
-          inputs.ollama-local-websearch.overlays.default
-        ];
-      }
-    ];
-  in {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      specialArgs = {
-        inherit
-          inputs
-          system
-          self
-          ;
-      };
-      modules =
-        [
+        modules = [
           ./hosts/nixos
           inputs.disko.nixosModules.disko
           inputs.preservation.nixosModules.default
         ]
         ++ commonModules;
+      };
     };
-  };
 }
