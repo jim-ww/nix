22c1b433bfa1c2f830136eec652b3f10312c4565
jim-ww <jim.w2610@proton.me>
2026-08-23 18:34:44 +0200

    debloat podman config

--- modules/podman.nix
+++ modules/podman.nix
@@ -1,6 +1,8 @@
 { pkgs, config, ... }: {
   environment.variables.PODMAN_COMPOSE_PROVIDER = "docker-compose";
 
+  environment.etc.timezone.text = config.time.timeZone;
+
   virtualisation.podman = {
     enable = true;
     dockerCompat = !config.virtualisation.docker.enable;
@@ -9,36 +11,7 @@
   };
 
   environment.systemPackages = with pkgs; [
-    podman-tui # status of containers in the terminal
-    docker-compose # start group of containers for dev
-    # dive # look into docker image layers
-    #podman-compose # start group of containers for dev
-    # podman-desktop
+    podman-tui
+    docker-compose
   ];
-
-  # systemd.user.services.podman-api = {
-  #   description = "Podman API Service";
-  #   requires = ["podman.socket"];
-  #   after = ["network.target" "podman.socket"];
-  #   documentation = ["man:podman-system-service(1)"];
-  #   startLimitIntervalSec = 0;
-  #   serviceConfig = {
-  #     Type = "exec";
-  #     ExecStart = "${pkgs.podman}/bin/podman system service --time=0 unix://%t/podman/podman.sock";
-  #     ExecStartPost = "${pkgs.podman}/bin/podman start --filter restart-policy=always --all";
-  #     Environment = "PODMAN_SOCKET=%t/podman/podman.sock";
-  #     KillMode = "process";
-  #     Restart = "on-failure";
-  #     RestartSec = 5;
-  #   };
-  #   wantedBy = ["default.target"];
-  # };
-
-  # systemd.user.sockets.podman = {
-  #   wantedBy = [ "sockets.target" ];
-  #   socketConfig = {
-  #     ListenStream = "%t/podman/podman.sock";
-  #     SocketMode = "0660";
-  #   };
-  # };
 }
