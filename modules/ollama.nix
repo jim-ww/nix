5e2a760941317b7a144653134bd12f1b8a5adf7a
jim-ww <jim.w2610@proton.me>
2026-08-23 20:03:10 +0200

    update ollama config

--- modules/ollama.nix
+++ modules/ollama.nix
@@ -12,7 +12,7 @@
   services.ollama = {
     enable = true;
     package = pkgs.ollama-vulkan;
-    #syncModels = true;  # remove models not specified below
+    syncModels = true; # remove models not specified below
     loadModels = [
       "hf.co/bartowski/L3-8B-Stheno-v3.2-GGUF:Q4_K_M" # RP, load 15.2-15.4s | prefill 38.1 t/s | gen 5.42 t/s
       "hf.co/mradermacher/Huihui-Qwen3.5-9B-abliterated-GGUF:Q4_K_M" # general purpose, thinking
@@ -23,9 +23,8 @@
       OLLAMA_NUM_THREADS = "8";
       OLLAMA_VULKAN = "1";
       OLLAMA_IGPU_ENABLE = "1";
-      OLLAMA_CLOUD_BASE_URL = "http://localhost:9080";
+      OLLAMA_NO_CLOUD = "1";
+      # OLLAMA_CLOUD_BASE_URL = "http://localhost:9080";
     };
-    # host = "0.0.0.0";
   };
-  # networking.firewall.allowedTCPPorts = [11434];
 }
