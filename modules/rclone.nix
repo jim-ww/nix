3975bcb823acd59ea6a5469ee95bed4726af705e
jim-ww <jim.w2610@proton.me>
2026-09-06 21:40:00 +0200

    rclone: add drive

--- modules/rclone.nix
+++ modules/rclone.nix
@@ -3,16 +3,32 @@
   sops.secrets = {
     rclone-mega-user.owner = config.user;
     rclone-mega-password.owner = config.user;
+    rclone-drive-token.owner = config.user;
+    rclone-drive-client-id.owner = config.user;
+    rclone-drive-client-secret.owner = config.user;
   };
 
   home-manager.users.${config.user}.programs.rclone = {
     enable = true;
-    remotes.mega = {
-      secrets = {
-        user = config.sops.secrets.rclone-mega-user.path;
-        pass = config.sops.secrets.rclone-mega-password.path;
+    remotes = {
+      mega = {
+        secrets = {
+          user = config.sops.secrets.rclone-mega-user.path;
+          pass = config.sops.secrets.rclone-mega-password.path;
+        };
+        config.type = "mega";
       };
-      config.type = "mega";
+      drive = {
+        secrets = {
+          token = config.sops.secrets.rclone-drive-token.path;
+          client_id = config.sops.secrets.rclone-drive-client-id.path;
+          client_secret = config.sops.secrets.rclone-drive-client-secret.path;
+        };
+        config = {
+          type = "drive";
+          scope = "drive";
+        };
+      };
     };
   };
 }
