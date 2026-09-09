ed00725e16c757e6bebeea07ab66a51201b2dace
jim-ww <jim.w2610@proton.me>
2026-09-10 00:45:14 +0200

    chore: rewrite minimal.nix as a USB rescue config

--- minimal.nix
+++ minimal.nix
@@ -1,89 +1,178 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 let
   user = "jim";
   hashedPassword = "$y$j9T$k9cTxhpl3769v0w3vtHHC.$RMnePBGaEHYBg3IZDSnGry3TBScXMfDpPAGXlM9EOJA";
   timezone = "Europe/Brussels";
+  term = "foot";
+  editor = "nvim";
+  browser = "librewolf";
 in
 {
-  # autologin
-  services.getty = {
-    autologinUser = user;
-    autologinOnce = true;
+  imports = [ ./hardware-configuration.nix ];
+
+  # ---- boot ----------------------------------------------------------------
+
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    efiInstallAsRemovable = true;
   };
-  environment.loginShellInit = ''
-    [[ "$(tty)" == /dev/tty1 ]] && sway
-  '';
+  boot.loader.timeout = 2;
 
-  virtualisation.podman.enable = true;
+  # ---- hardware ------------------------------------------------------------
 
-  programs.bash.blesh.enable = true;
+  hardware.graphics.enable = true;
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = false;
 
-  security.polkit.enable = true;
-  security.pam.services.swaylock = { };
-  programs.dconf.enable = true; # ?
+  services.libinput.touchpad.disableWhileTyping = true;
+  services.tlp.enable = true;
+  services.fstrim.enable = true;
+  zramSwap.enable = true;
+
+  # ---- networking ----------------------------------------------------------
+
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+  systemd.services.NetworkManager-wait-online.enable = false;
 
   services.openssh.enable = true;
-  services.dbus.enable = true;
-  services.libinput.touchpad.disableWhileTyping = true;
-  services.earlyoom.enable = true;
-  services.earlyoom.enableNotifications = true;
-  services.gnome.gnome-keyring.enable = true;
-  services.tlp.enable = true;
 
+  # ---- audio ---------------------------------------------------------------
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+  };
+
+  # ---- session -------------------------------------------------------------
   programs.sway = {
     enable = true;
-    extraSessionCommands = ''
-      mako &
-      swayidle &
-    '';
     extraPackages = with pkgs; [
       foot
-      imv
-      mpv
-      grim
-      mako
       rofi
-      libnotify
+      mako
       swaylock
-      swayidle # ?
+      swayidle
+      grim
+      slurp
       wl-clipboard
       brightnessctl
+      libnotify
+      imv
+      mpv
       zathura
       keepassxc
       librewolf-bin
     ];
   };
 
+  environment.etc."sway/config.d/local.conf".text = ''
+    set $menu rofi -show drun
+
+    input * {
+      xkb_layout us,ru
+      xkb_options grp:win_space_toggle
+      xkb_numlock enabled
+      tap enabled
+      accel_profile flat
+      pointer_accel 0.0
+    }
+
+    bindsym $mod+q exec $term
+    bindsym $mod+c kill
+    bindsym $mod+e exec $term -e lf
+    bindsym $mod+d exec $term -e nvim
+    bindsym $mod+f exec librewolf
+    bindsym $mod+b exec keepassxc
+    bindsym $mod+r exec $menu
+    bindsym $mod+v floating toggle
+    bindsym $mod+l exec swaylock -efkl
+    bindsym $mod+Shift+f fullscreen toggle
+    bindsym Print exec grim - | wl-copy
+
+    exec mako
+    exec swayidle -w timeout 600 'swaylock -efkl' before-sleep 'swaylock -efkl'
+
+    default_border pixel 1
+    gaps inner 4
+    smart_gaps on
+  '';
+
+  security.polkit.enable = true;
+  security.pam.services.swaylock = { };
+  programs.dconf.enable = true;
+
+  services.getty = {
+    autologinUser = user;
+    autologinOnce = false;
+  };
+
+  environment.loginShellInit = ''
+    [[ "$(tty)" == /dev/tty1 ]] && exec sway
+  '';
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.symbols-only
+    noto-fonts-cjk-sans
+  ];
+
+  # ---- packages ------------------------------------------------------------
+
   environment.systemPackages = with pkgs; [
-    lf
-    fzf
+    # cli
     neovim
     git
-    _7zz
-    btop
-    ncdu
+    lf
+    fzf
+    ripgrep
     tmux
+    btop
+    file
+    tree
+    gdu
+    wget
+    curl
+    _7zz
+    rsync
+
+    # disks and crypto, rescue
+    parted
+    gptfdisk
+    cryptsetup
+    btrfs-progs
+    dosfstools
+    e2fsprogs
+
+    # secrets
+    age
     sops
     gnupg
-    ripgrep
-    tealdeer
-    transmission_4
-    # optional
-    docker-compose
-    wormhole-william
-    gnumake
-    fastfetch-unwrapped
     gocryptfs
+
+    # net
     bluetuith
-    trashy
-    unison
-    nix-search
-    #steam-run-free
+    wormhole-william
+    nix-search-cli
   ];
+
+  # ---- shell ---------------------------------------------------------------
+
+  programs.bash.blesh.enable = true;
+
+  environment.variables = {
+    EDITOR = editor;
+    VISUAL = editor;
+    BROWSER = browser;
+    TERMINAL = term;
+    LESS = "-R";
+  };
 
   environment.shellAliases = {
     v = "$EDITOR";
     c = "clear";
+    l = "ls -h --group-directories-first --color=auto";
+    la = "ls -hA --group-directories-first --color=auto";
     conf = "$EDITOR /etc/nixos/configuration.nix";
     ns = "nix-search";
     nsp = "nix-shell -p";
@@ -91,45 +180,38 @@ in
     gs = "git status";
     ga = "git add";
     gaa = "git add --all";
-    gl = "git log";
-    gf = "git fetch";
-    gi = "git init";
-    gb = "git branch";
-    gsw = "git switch";
-    gd = "git diff";
+    gc = "git commit";
     gcm = "git commit -m";
+    gd = "git diff";
+    gl = "git log";
+    gsw = "git switch";
     gcl = "git clone";
     gps = "git push";
     gpl = "git pull";
-
-    bc = "busybox bc -q";
-    busybox = lib.getExe pkgs.busybox;
   };
 
-  fonts.packages = [ pkgs.nerd-fonts.symbols-only ];
+  # ---- users ---------------------------------------------------------------
 
-  environment.variables = {
-    PODMAN_COMPOSE_PROVIDER = "docker-compose";
-    TERMINAL = "foot";
-    EDITOR = "nvim";
-    LESS = "-R";
-    BROWSER = "librewolf";
-  };
-
-  xdg.portal = {
-    enable = true;
-    config.common.default = [
-      "wlr"
-      "gtk"
-    ];
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal-wlr
+  users.users.${user} = {
+    isNormalUser = true;
+    inherit hashedPassword;
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "video"
+      "audio"
     ];
   };
+
+  security.sudo.extraConfig = ''
+    Defaults lecture = never
+  '';
+
+  # ---- nix -----------------------------------------------------------------
+
+  nixpkgs.config.allowUnfree = true;
 
   nix.settings = {
-    auto-optimise-store = true;
     experimental-features = [
       "nix-command"
       "flakes"
@@ -143,38 +225,18 @@ in
     ];
   };
 
-  users.users.${user} = {
-    isNormalUser = true;
-    hashedPassword = hashedPassword;
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-    ];
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
   };
 
-  services.blueman.enable = true;
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = false;
+  services.earlyoom.enable = true;
 
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
+  # ---- locale --------------------------------------------------------------
 
   time.timeZone = timezone;
   i18n.defaultLocale = "en_US.UTF-8";
-
-  security.rtkit.enable = false;
-  services.pipewire.enable = true;
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  boot.loader.grub.enable = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.efiInstallAsRemovable = true;
-
-  system.copySystemConfiguration = true;
 
   system.stateVersion = "24.05";
 }
