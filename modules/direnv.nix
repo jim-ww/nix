27fe57e88d6cbf49f90ebb10fce7ffee3cb9b745
jim-ww <jim.w2610@proton.me>
2026-09-11 23:57:37 +0200

    direnv: source prebuilt bash hook

--- modules/direnv.nix
+++ modules/direnv.nix
@@ -1,6 +1,13 @@
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
   programs.direnv = {
     enable = true;
+    enableBashIntegration = false;
     nix-direnv.enable = true;
     config.load_dotenv = true;
     config = {
@@ -8,4 +15,8 @@
       warn_timeout = 0;
     };
   };
+
+  programs.bash.initExtra = lib.mkAfter ''
+    source ${pkgs.runCommand "direnv-hook.bash" { } "${lib.getExe config.programs.direnv.package} hook bash > $out"}
+  '';
 }
