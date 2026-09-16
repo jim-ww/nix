972d4161fd461e6fe6f33ec5c050718a76193f39
jim-ww <jim.w2610@proton.me>
2026-09-16 16:13:01 +0200

    add jrnl alias

--- aliases.nix
+++ aliases.nix
@@ -90,7 +90,7 @@ in
         restic = "restic --password-command 'sudo cat /run/secrets/restic-repo-password'";
         # shardic = "shardic --providers $(cat /run/secrets/shardic-providers)";
         snapshots-size = "sudo btrfs filesystem du -s /persistent/.snapshots/*/snapshot";
-
+        jrnl = ''SOPS_AGE_KEY="$(sudo cat $SOPS_AGE_KEY_FILE)" sops ${home}/Archive/personal/journal.md.age'';
         # unclutter home dir
         wget = ''${lib.getExe pkgs.wget} --hsts-file="${dataHome}/wget-hsts"'';
       };
